f3f295dedc8b0f5b9921e0269c5a7cf4fd06b5e4
Nery Lopez <nerya.lopezs.18@gmail.com>
2022-09-23 02:58:57 -0600

    2.1

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,26 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+/*Se importa el archivo Tablero.js que contiene el componente Tablero*/
 import Tablero from "./tablero";
 
 const Totito = () => {
   return (
     <div classNamee="container mx-auto ">
-      <div className="sm:grid grid-cols-2 w-screen h-screen bg-blue-400">
+
+      {/*La razon de agregar un div demas es para dividire el juego en dos secciones y que este se comporte de forma responsiva */}
+      <div className="sm:grid grid-cols-2 w-screen h-screen bg-indigo-600">
+
+        {/*Se llama al componente Tablero */}
         <Tablero className="m-5 p-2"/>
+
+        {/*Seccion pendiente... */}
+        <div className="flex justify-center">
+          <h1 className="mt-5 text-4xl font-bold text-white">TOTITO</h1>
+        </div>
       </div>
     </div>
   )
 }
 
-export default Totito
+export default Totito /*Se exporta el componente Totito */
