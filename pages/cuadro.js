1268476b9dbf20a871e1666236bc2dbb928cc919
Nery Lopez <nerya.lopezs.18@gmail.com>
2022-09-25 01:43:07 -0600

    Se agrega color rojo al boton ya jugado

--- pages/cuadro.js
+++ pages/cuadro.js
@@ -1,6 +1,6 @@
-const Cuadro = ({valor, alHacerClick}) => {
+const Cuadro = ({valor, alHacerClick, color}) => {
   return(
-    <button className="select-none shadow-lg shadow-gray-500  active:shadow-gray-900 active:shadow-inner active:bg-indigo-700 font-bold px-4 py-2 text-2xl sm:text-3xl xl:text-5xl text-white flex justify-center items-center transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+    <button className={color}
             onClick={() => alHacerClick()}>
       {valor}
     </button>
--- pages/tablero.js
+++ pages/tablero.js
@@ -1,26 +1,45 @@
 import { useState } from "react"
 import Cuadro from "./cuadro"
 
+const colorOriginal = "select-none shadow-lg shadow-gray-500  active:shadow-gray-900 active:shadow-inner active:bg-indigo-700 font-bold px-4 py-2 text-2xl sm:text-3xl xl:text-5xl text-white flex justify-center items-center transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+const colorError = "select-none shadow-lg shadow-gray-500  active:shadow-gray-900 active:shadow-inner active:bg-red-600 font-bold px-4 py-2 text-2xl sm:text-3xl xl:text-5xl text-white flex justify-center items-center transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-red-600 focus:ring-opacity-80"
+
 const Tablero = () => {
   const [cuadros, setCuadros] = useState(Array(9).fill(''))
   const [turno, setTurno] = useState('X')
-
+  const [error, setError] = useState(colorOriginal)
+  
   const pintaFigura = (indexItem) => {
     const misCuadritos = cuadros.slice()
     misCuadritos.splice(indexItem, 1, turno)
-    setCuadros(misCuadritos)
-    if(turno === 'X'){
-      setTurno('O')
+    setError(colorOriginal)
+
+    if(cuadros[indexItem] != ''){
+
+      setError(colorError)
+      
     } else{
-      setTurno('X')
+
+      setCuadros(misCuadritos)
+
+      if(turno === 'X'){
+        setTurno('O')
+      } else{
+        setTurno('X')
+      }
+
+      
     }
+    
   }
+
   return (
     <div className="p-2 xl:p-5 grid grid-cols-3 gap-2 xl:gap-5 bg-blue-200 h-72 sm:h-screen">{/*Tablero responsivo */}
       {
         cuadros.map((item, indexItem) => {
           return <Cuadro key={indexItem} 
                          valor={item} 
+                         color={error}
                          alHacerClick={() => pintaFigura(indexItem)} />
         })
       }
