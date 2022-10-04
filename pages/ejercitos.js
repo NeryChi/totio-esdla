c7bc59cc5192d9b078c474e85561c7e603b1192f
NeryChi <nerychi1@outlook.es>
2022-10-04 17:42:16 -0600

    Agregando Jugadores

--- pages/ejercitos.js
+++ pages/ejercitos.js
@@ -1,5 +1,23 @@
-const Ejercitos = () => {
-    return(
-        
-    )
+import { useState } from "react"
+import Ejercito from "./ejercito"
+
+const ejercitos = ['Rohan', 'Gondor', 'Mordor', 'Isengard']
+
+const Ejercitos = ({}) => {
+
+  //const [cuadros, setCuadros] = useState(Array(9).fill([]))
+  const [ejercito, setEjercito] = useState(Array(ejercitos.length).fill(''))
+
+  return(
+    <select className="w-4/5 ml-1 bg-gray-200 text-white font-bold text-2xl">
+        {
+          ejercito.map((item, itemIndex) => {
+            return <Ejercito key={itemIndex}
+                    ejercito = {ejercitos[itemIndex]} />
+          })
+        }
+    </select>   
+  )
 }
+
+export default Ejercitos
--- pages/tablero.js
+++ pages/tablero.js
@@ -1,6 +1,7 @@
 import { useState, useRef } from "react"
 import Cuadro from "./cuadro"
 import Jugador from "./jugador"
+import Ejercitos from "./ejercitos"
 
 const colorOriginal = "select-none shadow-lg shadow-gray-500  active:shadow-gray-900 active:shadow-inner active:bg-indigo-700 font-bold px-4 py-2 text-2xl sm:text-3xl xl:text-5xl text-white flex justify-center items-center transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
 const colorError = "select-none shadow-lg shadow-gray-500  active:shadow-gray-900 active:shadow-inner active:bg-red-600 font-bold px-4 py-2 text-2xl sm:text-3xl xl:text-5xl text-white flex justify-center items-center transition-colors duration-300 transform bg-red-600 rounded-md hover:bg-red-500 "
@@ -79,7 +80,6 @@ const Tablero = () => {
 
   return (
     <>
-      
       <div className="p-2 xl:p-5 grid grid-cols-3 gap-2 xl:gap-5 bg-blue-200 h-72 sm:h-screen">{/*Tablero responsivo */}
       {
         cuadros.map((item, indexItem) => {
@@ -95,6 +95,14 @@ const Tablero = () => {
       <div className="w-full flex">
         <div className="w-full z-10">
           <h1 className="w-full text-center mt-5 text-3xl md:text-4xl font-bold text-white">TOTITO <br/>The Lord Of The Rings</h1>
+          <br/>
+          <br/>
+          <div className="grid grid-cols-2 gap-2">
+            <h1 className="ml-5 text-white font-bold text-2xl">Jugador 1</h1>
+            <Ejercitos />
+            <h1 className="ml-5 text-white font-bold text-2xl">Jugador 2</h1>
+            <Ejercitos />
+          </div>
           <br/>
           <br/>
           <span className="ml-5 text-xl md:text-2xl font-bold text-white">{'El ganador es: ' + calcularGanador(cuadros)}</span>
